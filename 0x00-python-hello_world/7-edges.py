6f9a567586e9addc0ee42903adaec752d515d5f4
Joel Iziren <izirenjoel@gmail.com>
2023-11-27 13:09:18 -0800

    Complete source code to extract and print first 3, last 2, and middle letters

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3, *middle_word, word_last_2 = word
-print(f"First 3 letters: {''.join(word_first_3)}")
-print(f"Last 2 letters: {''.join(word_last_2)}")
-print(f"Middle word: {''.join(middle_word)}")
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
