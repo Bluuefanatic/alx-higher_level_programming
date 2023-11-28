571c53004ab1d27616e0032863c020a2651e5e2b
Joel Iziren <izirenjoel@gmail.com>
2023-11-28 04:19:55 -0800

    Refactor code to print lowercase ASCII alphabet with a single print and string format

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-# Print the ASCII alphabet in lowercase without a new line
+# Print the ASCII alphabet in lowercase without a new line using string format
 for i in range(97, 123):
-    print(chr(i), end="")
+    print("{}".format(chr(i)), end="")
