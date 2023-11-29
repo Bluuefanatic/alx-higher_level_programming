4f625974375336428323c797478725aec5f85920
Joel Iziren <izirenjoel@gmail.com>
2023-11-28 23:35:50 -0800

    Implement uppercase function to print a string in uppercase followed by a new line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,7 @@ def uppercase(s):
         # Check if the character is lowercase letter and convert to uppercase
         print(
             "{}".format(chr(ord(char) - 32))
-            if 97 <= ord(char) <= 122 
+            if 97 <= ord(char) <= 122
             else char,
             end=""
         )
