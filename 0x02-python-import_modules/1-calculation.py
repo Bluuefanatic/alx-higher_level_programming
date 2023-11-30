856c74a25cafe76ac5868eca580ca646187825bd
Joel Iziren <izirenjoel@gmail.com>
2023-11-30 06:20:00 -0800

    Add Python script for mathematical operations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+"""Print the sum, difference, multiple and quotient of 10 and 5."""
 from calculator_1 import add, sub, mul, div
 
 def main():
@@ -16,3 +16,6 @@ def main():
     print("{} - {} = {}".format(a, b, res_sub))
     print("{} * {} = {}".format(a, b, res_mul))
     print("{} / {} = {}".format(a, b, res_div))
+
+if __name__ == "__main__":
+    main()
