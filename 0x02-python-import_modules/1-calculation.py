1afcf9489529c6004c600215301813bc0d9a17c3
Joel Iziren <izirenjoel@gmail.com>
2023-11-30 06:48:13 -0800

    Import and use functions from calculator_1.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
 
-    res_add = add(a, b)
-    res_sub = sub(a, b)
-    res_mul = mul(a, b)
-    res_div = div(a, b)
-
-    print(res_add)
-    print(res_sub)
-    print(res_mul)
-    print(res_div)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
