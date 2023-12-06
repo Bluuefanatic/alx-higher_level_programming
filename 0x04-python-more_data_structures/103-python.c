e093828b94ab058fb35c45acb5c57d69d8a1daa2
Joel Iziren <izirenjoel@gmail.com>
2023-12-06 05:27:20 -0800

    Implement C functions for printing Python list and bytes information

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,40 +1,54 @@
 #include <Python.h>
 #include <stdio.h>
 
-void print_python_list(PyObject *p) {
-    Py_ssize_t size, i;
+/**
+ * print_python_list - Prints information about a Python list
+ * @p: Pointer to the Python object
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size, i;
 
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %ld\n", Py_SIZE(p));
-    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", Py_SIZE(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-    size = Py_SIZE(p);
-    for (i = 0; i < size; i++) {
-        printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-    }
+	size = Py_SIZE(p);
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
 }
 
-void print_python_bytes(PyObject *p) {
-    Py_ssize_t size, i;
-    char *bytes_str;
+/**
+ * print_python_bytes - Prints information about a Python bytes object
+ * @p: Pointer to the Python object
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size, i;
+	char *bytes_str;
 
-    printf("[.] Python bytes info\n");
-    if (!PyBytes_Check(p)) {
-        printf("  [ERROR] Invalid Bytes Object\n");
-        return;
-    }
+	printf("[.] Python bytes info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
 
-    size = PyBytes_Size(p);
-    printf("  Size: %ld\n", size);
-    printf("  trying string: %s\n", PyBytes_AsString(p));
+	size = PyBytes_Size(p);
+	printf("  Size: %ld\n", size);
+	printf("  trying string: %s\n", PyBytes_AsString(p));
 
-    bytes_str = PyBytes_AsString(p);
-    printf("  first %ld bytes: ", size + 1 > 10 ? 10 : size + 1);
-    for (i = 0; i < size + 1 && i < 10; i++) {
-        printf("%02hhx", bytes_str[i]);
-        if (i + 1 < size + 1 && i + 1 < 10) {
-            printf(" ");
-        }
-    }
-    printf("\n");
+	bytes_str = PyBytes_AsString(p);
+	printf("  first %ld bytes: ", size + 1 > 10 ? 10 : size + 1);
+	for (i = 0; i < size + 1 && i < 10; i++)
+	{
+		printf("%02hhx", bytes_str[i]);
+		if (i + 1 < size + 1 && i + 1 < 10)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
 }
