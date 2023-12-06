8fd3d2fcf5d8a0122f0b081b98a65b6fa49b2330
Joel Iziren <izirenjoel@gmail.com>
2023-12-06 04:56:51 -0800

    Implement only_diff_elements function for finding elements present in only one set

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix to store the squared values
-    result_matrix = []
-    
-    # Iterate through each row in the input matrix
-    for row in matrix:
-        # Create a new row for the result matrix
-        result_row = []
-        
-        # Iterate through each element in the row and square it
-        for value in row:
-            result_row.append(value ** 2)
-        
-        # Append the squared row to the result matrix
-        result_matrix.append(result_row)
-    
-    return result_matrix
+    """
+    wordA function that computes the square
+    value of all integers of a matrix.
+    """
+    new_matrix = []
+    for col in matrix:
+        result = list(map(lambda x: x**2, col))
+        new_matrix.append(result)
+    return new_matrix
