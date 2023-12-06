9d73e6a6f356aa02e86b51610a1426df45294e06
Joel Iziren <izirenjoel@gmail.com>
2023-12-06 04:34:40 -0800

    Implement simple_delete function for deleting keys in a dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    # Check if the key exists before attempting deletion
+    """
+    A function that deletes a key in a dictionary.
+    """
     if key in a_dictionary:
         del a_dictionary[key]
-        return a_dictionary
+    return a_dictionary
