fd905df23cd3bed2dd81c2b336fab00099c3224b
Bluuefanatic <izirenjoel@gmail.com>
2023-12-19 15:42:00 -0800

    Enhance Square class with optional size, type, and value validation in __init__

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 
-"""define a class square"""
+"""Define a class Square."""
 
 
 class Square:
-    """
-    This is the Square class.
-
-    Attributes:
-        __size (int): Private instance attribute representing the size of the square.
-    """
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """
-        Initializes a new instance of the Square class.
+        """Initialize a new Square.
 
-        Parameters:
-            size (int): The size of the square (default is 0).
+        Args:
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
