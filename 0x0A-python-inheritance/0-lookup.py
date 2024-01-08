9f37aac860226228c5d65cea86de58c6832466d9
Bluuefanatic <izirenjoel@gmail.com>
2024-01-08 13:10:40 -0800

    Add lookup function to retrieve attributes and methods of an object.

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
 def lookup(obj):
     """Return a list of available attributes and methods of an object."""
     return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or attr.startswith("__")]
