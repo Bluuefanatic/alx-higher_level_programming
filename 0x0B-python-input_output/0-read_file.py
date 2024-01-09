4453302eea38cc15422d6f58a858d634258afcdb
Bluuefanatic <izirenjoel@gmail.com>
2024-01-09 05:34:49 -0800

    Add read_file function to print UTF-8 text file to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ def read_file(filename=""):
     """Reads a text file and prints it to stdout.
 
     Args:
-        filename (str): The name of the file to read. Default is an empty string.
+        filename (str): The name of the file to read. Default is empty string.
     """
     with open(filename, encoding='utf-8') as file:
         for line in file:
