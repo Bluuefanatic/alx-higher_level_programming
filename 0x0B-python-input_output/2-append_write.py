d532c9c497e50a2f0287523fa9c3bb2e9805c6f2
Bluuefanatic <izirenjoel@gmail.com>
2024-01-09 14:27:36 -0800

    Implement append_write function for appending text to a file and returning character count

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a file-appending function."""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF-8)
+
+    Args:
+        filename (str): The name of the file to append.
+        text (str): The string to append to the file.
+
+    Returns:
+        int: The number of characters added to the file.
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        num_characters_added = file.write(text)
+    
+    return num_characters_added
