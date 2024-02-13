4e2e8f61f9a705da59809cbf7ce7d0487c426207
Bluuefanatic <izienjoel@gmail.com>
2024-02-13 06:05:57 -0800

    Add script to insert new row in specified database table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- insert_new_row.sql
+-- This script inserts a new row into the table first_table in the specified database
+
+-- Insert new row
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
