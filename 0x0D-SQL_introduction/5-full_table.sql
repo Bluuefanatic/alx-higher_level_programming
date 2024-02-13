a39108b35d4231e9675c6906dd530a60c011da8a
Bluuefanatic <izienjoel@gmail.com>
2024-02-13 06:00:46 -0800

    Add script to print table description in specified database

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,4 @@
 -- print_table_description.sql
 -- This script prints the full description of the table first_table in the specified database
 
--- Extract table information from information_schema
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = '$1' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
