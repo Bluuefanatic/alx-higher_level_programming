df920efc4c66cb2cde525e1ec8385d10637a1b32
Bluuefanatic <izienjoel@gmail.com>
2024-02-13 05:48:40 -0800

    Add script to list tables in specified database

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,7 @@
+-- list_tables.sql
+-- This script lists all tables in a specified MySQL database
+
+USE $1; -- Use the database passed as an argument
+
+-- Show all tables in the specified database
+SHOW TABLES;
