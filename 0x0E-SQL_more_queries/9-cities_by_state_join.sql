b2ccf08d39426eb8d2a5a4b343bb01e499c8f570
Bluuefanatic <izienjoel@gmail.com>
2024-02-14 05:25:40 -0800

    List all cities with their corresponding state names, sorted by cities.id

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- List all cities with their corresponding state names
+SELECT cities.id, cities.name, states.name as state_name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
