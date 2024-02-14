9e9deaf3ceb1ed45db14dba379bddc4b19486499
Bluuefanatic <izienjoel@gmail.com>
2024-02-14 05:04:58 -0800

    Create MySQL database hbtn_0d_2 and user_0d_2 with SELECT privilege

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Create or update database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create or update user_0d_2 with SELECT privilege on hbtn_0d_2
+CREATE OR REPLACE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
