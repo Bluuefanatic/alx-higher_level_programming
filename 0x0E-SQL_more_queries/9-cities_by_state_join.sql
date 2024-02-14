9853c67d267efdf9a9be55b2b720f327eaef4642
Bluuefanatic <izienjoel@gmail.com>
2024-02-14 05:27:18 -0800

    List all cities with their corresponding state names, sorted by cities.id

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,3 @@
--- List all cities with their corresponding state names
-SELECT cities.id, cities.name, states.name as state_name
-FROM cities, states
-WHERE cities.state_id = states.id
-ORDER BY cities.id ASC;
+-- lists all cities contained in the database hbtn_0d_usa
+-- lists all rows of a particular column in a database
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
