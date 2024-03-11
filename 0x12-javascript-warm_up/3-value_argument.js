f7b3c3cd1afb89391c115e583f01e9f83395c0b5
Bluuefanatic <izirenjoel@gmail.com>
2024-03-11 13:34:44 -0700

    Add script to print the first argument or 'No argument' if none provided

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
-const firstArg = process.argv[2];
-
-if (typeof firstArg === 'undefined') {
-    console.log('No argument');
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-    console.log(firstArg);
+  console.log(process.argv[2]);
 }
