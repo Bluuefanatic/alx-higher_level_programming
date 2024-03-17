2465216a9663ccd6de95b41db931f1320cf3d0d6
Bluuefanatic <izirenjoel@gmail.com>
2024-03-17 08:54:10 -0700

    Add City class definition and script to list all City objects from database using SQLAlchemy

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Defines the City class."""
-
+"""
+Contains the class definition of a City
+"""
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+
 
 class City(Base):
-    """The City class inherits from Base.
-    Links to the MySQL table cities.
-    Attributes:
-        id (Column): Represents a column of an auto-generated, unique integer, can't be null and is a primary key.
-        name (Column): Represents a column of a string with maximum 128 characters and can't be null.
-        state_id (Column): Represents a column of an integer, can't be null and is a foreign key to states.id.
+    """
+    Class that defines each city
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
