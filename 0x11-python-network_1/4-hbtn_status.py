edc0439274994160aa4aa643b8aee0ee85d7623c
root <izirenjoel@gmail.com>
2024-03-28 17:35:34 -0700

    Implement Python script using requests package to fetch URL response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status using requests package"""
+"""Fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
 if __name__ == "__main__":
