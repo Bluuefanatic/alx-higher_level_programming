cc8dd2f0a6d817b1e823b8848b7abba8ede19f56
root <izirenjoel@gmail.com>
2024-05-21 15:12:47 -0700

    Add script to write a string to a file with error handling

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-if (!filePath || !content) {
-  console.error('Error: File path and content to write must be provided');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, content, 'utf8', (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  } else {
-    console.log(`Successfully wrote to ${filePath}`);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
