f4b397d85337b8a4a335e2018a07f80cb3a9494a
root <izirenjoel@gmail.com>
2024-05-21 15:26:28 -0700

    This script makes a GET request to the Star Wars API endpoint for films, filters the results to count how many films include the character Wedge Antilles, and prints the count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,32 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-const apiUrl = process.argv[2];
-const characterId = '18';
-
-if (!apiUrl) {
-  console.error('Error: API URL must be provided');
-  process.exit(1);
-}
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: Status code ${response.statusCode}`);
-    process.exit(1);
-  } else {
-    const data = JSON.parse(body);
-    const films = data.results;
-    let count = 0;
-
-    films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        count++;
-      }
-    });
-
-    console.log(count);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
