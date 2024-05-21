544c275ff91619886bfa425791663ae0f38bb592
root <izirenjoel@gmail.com>
2024-05-21 15:07:46 -0700

    Add script to read and print file content with error handling

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error('Error: No file path provided');
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } else {
+    console.log(data);
+  }
+});
